6b3580d567ab0aad089aa935a4f7ff721da96674
treadmoon <mlvae@qq.com>
2023-01-18 15:38:14 +0800

    标题

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState, lazy, Suspense } from "react";
-import "./App.css";
-import { createdData, realData } from "./data";
+import { lazy } from 'react';
+import './App.css';
+import { realData } from './data';
 
-const Chart = lazy(async () => import("./views/Chart"));
+const Chart = lazy(async () => import('./views/Chart'));
 
 function App() {
   return (
     <div className="App">
+      <h2>组织结构图</h2>
       <Chart data={realData} />
     </div>
   );
