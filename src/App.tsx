c3a4053564b326cf440dd0b340988ac35c222de1
treadmoon <mlvae@qq.com>
2023-01-31 16:11:34 +0800

    修改结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { lazy } from 'react';
 import './App.css';
 import { realData } from './data';
 
-const Chart = lazy(async () => import('./views/Chart'));
+import { Chart } from "../lib/index"
 
 function App() {
   return (
     <div className="App">
-      <h2>组织结构图</h2>
       <Chart data={realData} />
     </div>
   );
