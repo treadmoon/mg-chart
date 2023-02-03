6a98c8d05094d4ab102536b8b926b842e3c26f9b
treadmoon <mlvae@qq.com>
2023-02-03 08:40:04 +0800

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css';
 import { realData } from './data';
 
-import { Chart } from "../dist/my-lib.mjs"
+// import { Chart } from "../dist/my-lib.mjs"
+import { Chart } from "../lib/index"
 import "../dist/style.css"
 
 function App() {
